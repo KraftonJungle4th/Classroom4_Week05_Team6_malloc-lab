74405d6900110d67677ecd71df997a4ebf8f880a
moonung <ansdj3523@gmail.com>
2024-02-17 19:46:12 +0900

    making init

--- moon/mm.c
+++ moon/mm.c
@@ -44,12 +44,67 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<<12)
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
+#define PACK(size, alloc) ((size) | (alloc))
+
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p,val) (*(unsigned int *)(p) = (val))
+
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(P) & 0x1)
+
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - WSIZE)
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV-BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+static char *heap_listp;
+
+
+
+
 /* 
  * mm_init - initialize the malloc package.
  */
+//메모리 관리 시스템을 초기화하는 함수
 int mm_init(void)
+
 {
+    if((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));
+    PUT(heap_listp + (3*WSIZE), PACK(0, 1));
+
+    heap_listp += (2*WSIZE);
+
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
     return 0;
+}
+
+// 메모리힙을 확장하는 역할
+static void *extend_heap(size_t words)
+{
+	char *bp;
+	size_t size;
+	size = (words%2) ? (words+1)*WSIZE : words*WSIZE;
+    
+	if((long)(bp = mem_sbrk(size)) == -1)
+		return NULL;
+
+	PUT(HDRP(bp), PACK(size, 0)); // Free block header
+	PUT(FTRP(bp), PACK(size, 0)); // Free block footer
+	PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1)); // New epilogue header
+
+	return coalesce(bp);
 }
 
 /* 
